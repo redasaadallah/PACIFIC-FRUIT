1b1db086e6153e6e4c02fcc98ee8499ae1c4c1bb
redasaadallah <redasaadallah77@gmail.com>
2026-04-07 13:42:41 +0100

    first importation

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Splash from "./composants/splash"
+import React,{useContext,useState,useEffect} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // ← make sure to import these
+import ProtectedRoute from "./ProtectedRoute";
+import Loader from "./composants/loader"
+import "./App.css"
+
+import Home from "./home"
+import Apropos from "./apropos"
+function App() {
+  const[splash,setsplash]=useState(0);
+      useEffect(()=>{
+          setTimeout(()=>{
+              setsplash(1);
+          },4000);
+      },[]);
+  const [step, setstep]=useState(1);
+  return (
+    <>
+    <Routes>
+    <Route path="/" element={<Splash/>} />
+    <Route path="/home" element={<Home/>} />
+        <Route path="/apropos" element={<Apropos/>} />
+
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/composants/header.js
@@ -0,0 +1,63 @@
+import "../styles/header.css"
+import france from "../img/france.png"
+import arow from "../img/arowupdown.png"
+import logo from "../img/logo.png"
+import logoo from "../img/logoo.png"
+import menu from "../img/menu.png"
+import close from "../img/close.png"
+import homefilled from "../img/home.png"
+import homevide from "../img/homevide.png"
+import info from "../img/info.png"
+import infofilled from "../img/infofilled.png"
+import contact from "../img/contact.png"
+import booking from "../img/booking.png"
+import cancel from "../img/cancel.png"
+import { useState } from "react"
+import {useNavigate} from "react-router-dom"
+
+function Header({at,atphone}){
+    const [show,setshow]=useState(0);
+    const [showR,setshowR]=useState(0);
+    const navigate=useNavigate();
+    
+    return(<>
+    <div id="header">
+        <div ></div>
+        <img src={logo} alt=""/>
+        <img onClick={()=>{setshow(1)}} src={menu} alt=""/>
+        {showR===1 && <div id="menuR"><div></div><img onClick={()=>{setshowR(0);}} src={close} /><div></div><button>Accéder à ma réservation</button><button>Réserver un espace</button></div>}
+        <div>
+        <button onClick={()=>{at!==0 && navigate("/home")}} style={at === 0 ? { color: "#0A8D47",borderBottom:"solid 2px #0A8D47" } : {}}>Accueil</button>
+        <button onClick={()=>{navigate("/apropos")}} style={at === 1 ? { color: "#0A8D47",borderBottom:"solid 2px #0A8D47" } : {}}>À propos</button>
+        <button style={at === 2 ? { color: "#0A8D47",borderBottom:"solid 2px #0A8D47" } : {}}>Réservation</button>    
+        <button style={at === 3 ? { color: "#0A8D47",borderBottom:"solid 2px #0A8D47" } : {}}>Contact</button>
+        </div>
+        <button><img src={france} alt=""/>France <img src={arow} alt=""/></button>
+     </div>
+     {/* ===================================================== */}
+     <div className={show===0?"headermenu":"showmenu"}>
+        <div>
+        </div>
+        <div >
+            <div><img onClick={()=>{setshow(0);}} src={close}/></div>
+            <img src={logoo}/>
+            <div>
+                <button onClick={()=>{atphone!==0 && navigate("/home")}} id={atphone===0?"atphone":"natphone"} ><img src={atphone===0?homefilled:homevide}/>Accueil</button>
+                <button onClick={()=>{atphone!==1 && navigate("/apropos")}} id={atphone===1?"atphone":"natphone"}><img src={atphone===1?infofilled:info}/>À propos</button>
+                <button onClick={()=>{setshowR(1)}}><img src={booking}/>Réservation</button>
+                <button><img src={contact}/>Contact</button>
+                {showR===1&&
+                <div>
+                    <img onClick={()=>{setshowR(0)}} src={cancel}/>
+                    <button>Accéder à ma réservation</button>
+                    <button>Réserver un espace</button>
+                </div>}
+            </div>
+            <button><img src={france} alt=""/>France <img src={arow} alt=""/></button>
+
+        </div>
+     </div>
+     </>
+     );
+}
+export default Header;
